9408532ff25116c976d071f736ea0c1ea502327f
DeMarkiz <markberezhnoy27@gmail.com>
2024-06-23 22:47:37 +0300

    init

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,33 @@
 from datetime import datetime
 
 
-def filter_by_state(slovar, state='EXECUTED'):
-    my_list = []
-    for i in slovar:
-        if i.get('state') == state:
-            my_list.append(i)
-    return my_list
+def filter_by_state(records: list, state: str = "EXECUTED") -> list:
+    """
+    Фильтрует операции по заданному состоянию.
+
+    :param records: Список операций.
+    :param state: Состояние для фильтрации (по умолчанию 'EXECUTED').
+    :return: Отфильтрованный список операций.
+    """
+    return [record for record in records if state == record.get("state")]
 
 
-def sort_by_date(slovar, ascending = True):
-    def sort_key(record):
+def sort_by_date(records: list, ascending: bool = True) -> list:
+    """
+    Сортирует операции по возрастанию (по умолчанию).
+
+    :param records: Список операций.
+    :param ascending: Параметр для сортировки по дате (по умолчанию True - сортировка по возростанию).
+    :return: Отсортированный список операций.
+    """
+
+    def sort_key(record: dict) -> datetime:
+        """
+        Используется в качестве ключа сортировки в функции sorted(), извлекая дату из записи.
+
+        :param record: Запись, содержащая дату и время в формате ISO 8601.
+        :return: Объект datetime, полученный из строки даты, который используется в качестве ключа сортировки.
+        """
         return datetime.strptime(record["date"], "%Y-%m-%dT%H:%M:%S.%f")
-    return sorted(slovar, key=sort_key, reverse=not ascending)
+
+    return sorted(records, key=sort_key, reverse=not ascending)
